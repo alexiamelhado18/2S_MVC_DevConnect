f33d2551056891c9dc1b6d77e4f7942a6f1bd15d
Alexia Vitoria Melhado Pereira <alexia.vitoria@sp.senai.br>
2025-12-12 08:40:51 -0300

    iniciando mvc do devconnect com a turma moura

---
+++ Contexts/DevConnectContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using DevConnectMoura.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevConnectMoura.Contexts;
+
+public partial class DevConnectContext : DbContext
+{
+    public DevConnectContext()
+    {
+    }
+
+    public DevConnectContext(DbContextOptions<DevConnectContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TbComentario> TbComentario { get; set; }
+
+    public virtual DbSet<TbCurtida> TbCurtida { get; set; }
+
+    public virtual DbSet<TbPublicacao> TbPublicacao { get; set; }
+
+    public virtual DbSet<TbUsuario> TbUsuario { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DevCon_SA");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TbComentario>(entity =>
+        {
+            entity.HasKey(e => e.IdComentario).HasName("PK__tb_comen__1BA6C6F4FDFC8203");
+
+            entity.HasOne(d => d.IdPublicacaoNavigation).WithMany(p => p.TbComentario)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__tb_coment__id_pu__4316F928");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.TbComentario)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__tb_coment__id_us__4222D4EF");
+        });
+
+        modelBuilder.Entity<TbCurtida>(entity =>
+        {
+            entity.HasKey(e => e.IdCurtida).HasName("PK__tb_curti__0AC033CA6E86A5E0");
+
+            entity.HasOne(d => d.IdPublicacaoNavigation).WithMany(p => p.TbCurtida)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__tb_curtid__id_pu__46E78A0C");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.TbCurtida)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__tb_curtid__id_us__45F365D3");
+        });
+
+        modelBuilder.Entity<TbPublicacao>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tb_publi__3213E83F8A028041");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.TbPublicacao).HasConstraintName("FK__tb_public__id_us__3B75D760");
+        });
+
+        modelBuilder.Entity<TbUsuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tb_usuar__3213E83F334C96FC");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// CADASTRO DE USUÁRIO
+namespace DevConnectMoura.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ILogger<UsuarioController> _logger;
+
+        public UsuarioController(ILogger<UsuarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Cadastro de usuário
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Perfil do usuário
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
